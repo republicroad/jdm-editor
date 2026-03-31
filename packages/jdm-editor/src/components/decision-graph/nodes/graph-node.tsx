1ebddccc303b897b9152aea8c0a9e3183bd18eca
代双 <daishuang@geetest.com>
2026-03-31 15:49:09 +0800

    节点复制剪贴板& function UI bug 修复“

--- packages/jdm-editor/src/components/decision-graph/nodes/graph-node.tsx
+++ packages/jdm-editor/src/components/decision-graph/nodes/graph-node.tsx
@@ -75,16 +75,16 @@ export const GraphNode = React.forwardRef<HTMLDivElement, GraphNodeProps>(
       //     }
       //   : null,
       specification.documentationUrl ? { key: 'divider-1', type: 'divider' } : null,
-      // !displayError && {
-      //   key: 'copy-clipboard',
-      //   label: <SpacedText left='复制' right={platform.shortcut('Ctrl + C')} />,
-      //   onClick: () => graphActions.copyNodes([id]),
-      // },
       !displayError && {
         key: 'duplicate',
         disabled,
         label: <SpacedText left={t("copy")} right={platform.shortcut('Ctrl + D')} />,
         onClick: () => graphActions.duplicateNodes([id]),
+      },
+      !displayError && {
+        key: 'copy-clipboard',
+        label: <SpacedText left={t('copyToClipboard')} right={platform.shortcut('Ctrl + C')} />,
+        onClick: () => graphActions.copyNodes([id]),
       },
       !displayError && { key: 'divider-2', type: 'divider' },
       {
