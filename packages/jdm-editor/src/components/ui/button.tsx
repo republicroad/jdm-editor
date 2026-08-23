0c81356f2b7b3df90089c2cd8689dbf0c79a63a8
RYefccd <847960106@qq.com>
2026-08-23 17:31:16 +0800

    fix: forwardRef on ui Button so radix asChild triggers anchor popups correctly

--- packages/jdm-editor/src/components/ui/button.tsx
+++ packages/jdm-editor/src/components/ui/button.tsx
@@ -38,17 +38,10 @@ const buttonVariants = cva(
   }
 )
 
-function Button({
-  className,
-  variant = "default",
-  size = "default",
-  asChild = false,
-  ref,
-  ...props
-}: React.ComponentProps<"button"> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-  }) {
+const Button = React.forwardRef<
+  HTMLButtonElement,
+  React.ComponentProps<"button"> & VariantProps<typeof buttonVariants> & { asChild?: boolean }
+>(function Button({ className, variant = "default", size = "default", asChild = false, ...props }, ref) {
   const Comp = asChild ? Slot.Root : "button"
 
   return (
@@ -61,6 +54,7 @@ function Button({
       {...props}
     />
   )
-}
+})
+Button.displayName = "Button"
 
 export { Button, buttonVariants }
